cf4e2b365e5a6e570c37dbed2ba31d1fcf87f124
Madson Cordeiro <madson_cordeiro@hotmail.com>
2026-07-28 17:10:33 -0300

    Adicionando arquivo da Aula 1 do Módulo 1 contendo conteúdos abordados em aula

---
+++ Modulo1_Fundamentos_da_Linguagem/Aula1_Sintaxe_Variaveis_e_Tipos_Primitivos/1.1-variaveis-sintaxe-e-tipos-primitivos.py
@@ -0,0 +1,44 @@
+nome = "Luiza" # str (string)
+idade = 25 # int (integer)
+estado = "Pernambuco" # str (string)
+tem_cachorro = True # bool (boolean)
+altura = 1.58 # float (float)
+
+print(type(nome))
+print(type(idade))
+print(type(estado))
+print(type(tem_cachorro))
+print(type(altura))
+
+idade = 30
+print(idade)
+
+idade = "Luiza Vieira"
+print(idade)
+
+# nome_para_identificar = valor / conteúdo
+# Sempre coloque nomes fáceis de identificar do que essa variável se trata / conteúdo que ela guarda
+
+# Regras na criação das variáveis
+# 1. Não pode começar com um número.
+# 1idade_luiza = 14
+
+#2. Não pode ter um espaço.
+
+nome_sobrenome = "Luiza Vieira" # snake_case
+nomeSobrenome = "Luiza Vieira" # camelCase
+
+# 3. Python é case-sensitive
+
+cachorro = "Snoppy"
+Cachorro = "Bobby"
+
+# 4. Palavras-chaves que não podem ser nomes de variáveis
+# if, class, for, False, None, and, True, print, import, return, try, as, await, break, or, not
+
+# 5. Não pode conter caracteres especiais (exceto _)
+# @, $, !, ?
+
+# Comentários e Sintaxe Básica
+# Tudo que começa com hashtag (#) é um comentário no Python --> Ignorado durante execução
+# Executa tudo de cima para baixo, linha por linha
